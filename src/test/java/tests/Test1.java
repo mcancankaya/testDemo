28d9741be441ee7b30d7676a471c68e03d7c5ee1
mcancankaya <mccankaya45@hotmail.com>
2022-12-07 06:59:33 +0300

    Initial commit

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Test1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.ebebek.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+
+        WebElement searchBox = driver.findElement(By.id("txtSearchBox"));
+        searchBox.sendKeys("kaşık maması");
+        searchBox.sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+
+
+    }
+}
